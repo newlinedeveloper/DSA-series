6a5845cf7a8997675557769149f72dc3797cfeda
Veerasolaiyappan <60597798+newlinedeveloper@users.noreply.github.com>
2023-01-16 12:45:20 +0530

    Time: 53 ms (74.35%), Space: 14.5 MB (34.22%) - LeetHub

---
+++ 0242-valid-anagram/0242-valid-anagram.py
@@ -0,0 +1,18 @@
+class Solution:
+    def isAnagram(self, s: str, t: str) -> bool:
+        
+        if len(s)!=len(t):
+            return False
+        
+        sHash = {}
+        tHash = {}
+        
+        for i in range(len(s)):
+            sHash[s[i]] = 1 + sHash.get(s[i],0)
+            tHash[t[i]] = 1 + tHash.get(t[i],0)
+        
+        for i in sHash.keys():
+            if sHash.get(i) != tHash.get(i):
+                return False
+        
+        return True
